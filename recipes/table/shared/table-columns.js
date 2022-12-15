a4d578e410ee572fefa983097bc456e2333453f6
Nichols, Kieran <kieran.nichols@tylertech.com>
2022-12-15 13:40:37 -0500

    feat: add table recipes

---
+++ recipes/table/shared/table-columns.js
@@ -0,0 +1,6 @@
+window.ForgeTableColumns = [
+  { property: "Date", header: "Date" },
+  { property: "billNumber", header: "Bill number" },
+  { property: "readDate", header: "Read date" },
+  { property: "usage", header: "Usage" }
+];
---
+++ recipes/table/shared/table-data.js
@@ -0,0 +1,8 @@
+window.ForgeTableData = [
+  { Id: 1, Date: "06/15/2019", billNumber: "231", readDate: "06/10/2019", usage: "29.1" },
+  { Id: 2, Date: "05/15/2019", billNumber: "230", readDate: "05/10/2019", usage: "27.5" },
+  { Id: 3, Date: "04/15/2019", billNumber: "229", readDate: "04/10/2019", usage: "31.0" },
+  { Id: 4, Date: "03/15/2019", billNumber: "228", readDate: "03/10/2019", usage: "31.0" },
+  { Id: 5, Date: "02/15/2019", billNumber: "227", readDate: "02/10/2019", usage: "28.7" },
+  { Id: 5, Date: "01/15/2019", billNumber: "226", readDate: "01/10/2019", usage: "29.1" }
+];
